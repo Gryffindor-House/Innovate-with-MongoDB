5838f93707ac5a7194734c63fb7225b8b0b3585a
sinking8 <ashwinblaze111@gmail.com>
2022-01-12 21:25:03 +0530

    updated index.html

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,11 @@
 import "./App.css";
 
-import Footer from "./components/footer";
 import LoginForm from "./components/loginform";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Footer />
-        <LoginForm />
-      </header>
+      <LoginForm />
     </div>
   );
 }
