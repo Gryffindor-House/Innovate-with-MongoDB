eb0c3d8de65ace14e045094865d7c5b50df53bd0
sshreyas267 <sshreyas267@gmail.com>
2022-01-12 21:46:40 +0530

    added nav in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,12 @@ import "./App.css";
 
 import LoginForm from "./components/loginform";
 import Footer from "./components/footer";
+import Nav from "./components/nav";
 
 function App() {
   return (
     <div className="App">
+      <Nav />
       <LoginForm />
       <Footer />
     </div>
