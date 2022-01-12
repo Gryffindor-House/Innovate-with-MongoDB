f982b8195af6a649b41df38881a3d5ad329a1a83
S Neeraj <69386524+NeerajShyamSundar@users.noreply.github.com>
2022-01-12 21:42:43 +0530

    footer

--- client/src/components/footer.jsx
+++ client/src/components/footer.jsx
@@ -1,7 +1,26 @@
 import React, { Component } from "react";
 
-function Footer() {
-  return <footer>This is Footer</footer>;
+function Footer() 
+{
+  return (
+
+<body>
+    <div class="footer">
+        <footer>
+            <ul class="list-inline">
+                <li class="list-inline-item"><a href="#">Home</a></li>
+                <li class="list-inline-item"><a href="#">Services</a></li>
+                <li class="list-inline-item"><a href="#">About Our Team</a></li>
+            </ul>
+            <p class="copyright">Team Gryffindor</p>
+        </footer>
+    </div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/js/bootstrap.bundle.min.js"></script>
+</body>
+
+
+  )
 }
 
 export default Footer;
