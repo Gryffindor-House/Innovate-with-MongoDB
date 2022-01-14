3a45fa4cfa9bdedb67eb52a50ace829da8f4bcdc
sshreyas267 <sshreyas267@gmail.com>
2022-01-14 23:01:58 +0530

    Added about

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,13 @@ import "./App.css";
 import LoginForm from "./components/loginform";
 import Footer from "./components/footer";
 import Nav from "./components/nav";
+import About from "./components/about";
 
 function App() {
   return (
     <div className="App">
       <Nav />
+      <About />
       <LoginForm />
       <Footer />
     </div>
