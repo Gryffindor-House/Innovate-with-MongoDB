9b6d45443b752a89ebc36a17e06bc0fbdd34cab0
sshreyas267 <sshreyas267@gmail.com>
2022-01-17 22:46:42 +0530

    copied components,config and context

---
+++ client/my-app/src/components/homepage/home.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import About from "./about";
+import LoginForm from "./loginform";
+import Nav from "./nav";
+import Footer from "./footer";
+
+export const Home = () => {
+  return (
+    <>
+      <Nav />
+      <div className="flex">
+        <About />
+      </div>
+      <div className="flex">
+        <LoginForm />
+      </div>
+      <Footer />
+    </>
+  );
+};
