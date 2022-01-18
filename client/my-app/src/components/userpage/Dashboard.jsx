085875532dc1017a19c6f6fc4459ad695271be47
sinking8 <ashwinblaze111@gmail.com>
2022-01-19 02:40:42 +0530

    removed components

--- client/my-app/src/components/userpage/Dashboard.jsx
+++ client/my-app/src/components/userpage/Dashboard.jsx
@@ -1,8 +1,8 @@
-import React from "react";
-import { Button } from "react-bootstrap";
-import { useNavigate } from "react-router";
-import { useUserAuth } from "../../context/UserAuthContext";
-import Wreckinfo from "./wreckinfo";
+import React from 'react';
+import { Button } from 'react-bootstrap';
+import { useNavigate } from 'react-router';
+import { useUserAuth } from '../../context/UserAuthContext';
+import Wreckinfo from './wreckinfo';
 
 export const Dashboard = () => {
   const { logOut, user } = useUserAuth();
@@ -10,18 +10,15 @@ export const Dashboard = () => {
   const handleLogout = async () => {
     try {
       await logOut();
-      navigate("/");
+      navigate('/');
     } catch (error) {
       console.log(error.message);
     }
   };
   return (
     <>
-      {" "}
       <h1>Dashboard</h1>;
-
       <Wreckinfo />
-      
       <div className="d-grid gap-2">
         <Button variant="primary" onClick={handleLogout}>
           Log out
