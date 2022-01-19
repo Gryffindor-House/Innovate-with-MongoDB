d8b081246a523ee05bea1f8e1743f50e23ec0804
sinking8 <ashwinblaze111@gmail.com>
2022-01-19 22:39:32 +0530

    added navbar

--- client/my-app/src/components/userpage/Dashboard.jsx
+++ client/my-app/src/components/userpage/Dashboard.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Box, Grid, GridItem } from '@chakra-ui/react';
+import { Button, Box, Grid, GridItem, VStack } from '@chakra-ui/react';
 
 import Wreckinfo from './wreckinfo';
 import Map from './map';
@@ -8,9 +8,9 @@ import Navbar from './navbar';
 export const Dashboard = () => {
   return (
     <>
-      <Navbar />
       <Grid templateColumns="repeat(5, 1fr)" gap={1}>
-        <GridItem h="10" colStart={2} colEnd={3}>
+        <Navbar />
+        <GridItem h="10" colStart={2} colEnd={3} p={'12px'}>
           <Map />
         </GridItem>
         <GridItem colStart={3} colEnd={5} colSpan={2} h="10">
