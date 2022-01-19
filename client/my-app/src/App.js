8de6ebc59e73773f77b1e34dd47a4e865032cbf4
sinking8 <ashwinblaze111@gmail.com>
2022-01-19 14:18:03 +0530

    add fonts config files

--- client/my-app/src/App.js
+++ client/my-app/src/App.js
@@ -1,13 +1,14 @@
-import { Home } from "./components/homepage/home";
-import ProtectedRoute from "./components/routes/ProtectedRoute";
-import { Routes, Route } from "react-router-dom";
-import { UserAuthContextProvider } from "./context/UserAuthContext";
-import { Dashboard } from "./components/userpage/Dashboard";
-import { ChakraProvider } from '@chakra-ui/react'
+import { Home } from './components/homepage/home';
+import ProtectedRoute from './components/routes/ProtectedRoute';
+import { Routes, Route } from 'react-router-dom';
+import { UserAuthContextProvider } from './context/UserAuthContext';
+import { Dashboard } from './components/userpage/Dashboard';
+import { ChakraProvider } from '@chakra-ui/react';
+import theme from './config/fonts/font';
 
 function App() {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <UserAuthContextProvider>
         <Routes>
           <Route path="/" element={<Home />} />
@@ -21,7 +22,7 @@ function App() {
           />
         </Routes>
       </UserAuthContextProvider>
-      </ChakraProvider>
+    </ChakraProvider>
   );
 }
 
---
+++ client/my-app/src/config/fonts/font.js
@@ -0,0 +1,10 @@
+import { extendTheme } from '@chakra-ui/react';
+
+const theme = extendTheme({
+  fonts: {
+    heading: `Inter,-apple-system,BlinkMacSystemFont,"Segoe UI",Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol"`,
+    body: `Inter,-apple-system,BlinkMacSystemFont,"Segoe UI",Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol"`,
+  },
+});
+
+export default theme;
