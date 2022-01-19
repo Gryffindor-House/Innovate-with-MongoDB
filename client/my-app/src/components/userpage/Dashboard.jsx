dc703b7a58517a79b630c58803096a5be42bad58
sinking8 <ashwinblaze111@gmail.com>
2022-01-19 12:43:17 +0530

    added maps sample

--- client/my-app/src/components/userpage/Dashboard.jsx
+++ client/my-app/src/components/userpage/Dashboard.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { Button } from 'react-bootstrap';
+import { Button, Box, Grid, GridItem } from '@chakra-ui/react';
 import { useNavigate } from 'react-router';
 import { useUserAuth } from '../../context/UserAuthContext';
 import Wreckinfo from './wreckinfo';
+import Map from './map';
 
 export const Dashboard = () => {
   const { logOut, user } = useUserAuth();
@@ -18,9 +19,17 @@ export const Dashboard = () => {
   return (
     <>
       <h1>Dashboard</h1>;
-      <Wreckinfo />
+      <Grid templateColumns="repeat(5, 1fr)" gap={1}>
+        <GridItem h="10" colStart={2} colEnd={3}>
+          <Map />
+        </GridItem>
+
+        <GridItem colStart={3} colEnd={5} colSpan={2} h="10">
+          <Wreckinfo />
+        </GridItem>
+      </Grid>
       <div className="d-grid gap-2">
-        <Button variant="primary" onClick={handleLogout}>
+        <Button onClick={handleLogout} colorScheme="blue">
           Log out
         </Button>
       </div>
