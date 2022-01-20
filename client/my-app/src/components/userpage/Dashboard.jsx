2a9c8dd73ffdc74e706b6e1c5d460c2dbfff955d
sshreyas267 <sshreyas267@gmail.com>
2022-01-20 12:21:06 +0530

    Added infobox and resized components

--- client/my-app/src/components/userpage/Dashboard.jsx
+++ client/my-app/src/components/userpage/Dashboard.jsx
@@ -4,17 +4,21 @@ import { Button, Box, Grid, GridItem, VStack } from '@chakra-ui/react';
 import Wreckinfo from './wreckinfo';
 import Map from './map';
 import Navbar from './navbar';
+import Infobox from './infobox';
 
 export const Dashboard = () => {
   return (
     <>
       <Grid templateColumns="repeat(5, 1fr)" gap={1}>
         <Navbar />
-        <GridItem h="10" colStart={2} colEnd={3} p={'55px'}>
+        <GridItem h="15" colStart={2} colEnd={3} p={'25px'}>
           <Map />
         </GridItem>
         <GridItem colStart={3} colEnd={5} colSpan={2} h="10">
           <Wreckinfo />
+        </GridItem>
+        <GridItem rowStart={2} colStart={2} colEnd={3} colSpan={2} h="10">
+        <Infobox />
         </GridItem>
       </Grid>
     </>
