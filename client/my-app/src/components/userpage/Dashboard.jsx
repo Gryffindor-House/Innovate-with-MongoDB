f0eb213a6cbc678b3a475aef4a3f640f1a0ec692
sshreyas267 <sshreyas267@gmail.com>
2022-01-22 00:49:37 +0530

    Changed grid

--- client/my-app/src/components/userpage/Dashboard.jsx
+++ client/my-app/src/components/userpage/Dashboard.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Box, Grid, GridItem, VStack } from '@chakra-ui/react';
+import { Button, Box, Grid, GridItem, VStack, grid } from '@chakra-ui/react';
 
 import Wreckinfo from './wreckinfo';
 import Map1 from './map';
@@ -10,10 +10,10 @@ import Infobox from './infobox';
 export const Dashboard = () => {
   return (
     <>
-    <GridItem h="15" colStart={2} colEnd={3} pt={'50px'}>
+    <Grid>
       <Navbar />
         
-      </GridItem>
+      </Grid>
     </>
   );
 };
