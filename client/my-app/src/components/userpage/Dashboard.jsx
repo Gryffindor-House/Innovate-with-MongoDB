46a9d7cffa38a03e53c8fdddd645a2c308fd4a02
sinking8 <ashwinblaze111@gmail.com>
2022-01-22 00:47:38 +0530

    updated

--- client/my-app/src/components/userpage/Dashboard.jsx
+++ client/my-app/src/components/userpage/Dashboard.jsx
@@ -2,17 +2,16 @@ import React from 'react';
 import { Button, Box, Grid, GridItem, VStack } from '@chakra-ui/react';
 
 import Wreckinfo from './wreckinfo';
-import Map from './map';
 import Navbar from './navbar';
 import Infobox from './infobox';
-  
+import GoogleMap from './map';
 
 export const Dashboard = () => {
   return (
     <>
       <Grid gap={1}>
         <Navbar />
-        
+        <GoogleMap />
       </Grid>
     </>
   );
