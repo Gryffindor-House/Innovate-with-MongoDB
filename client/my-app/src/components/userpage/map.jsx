c2801a29f4fc566c0fc9f3db8873dc6aeddf2cad
sinking8 <ashwinblaze111@gmail.com>
2022-01-22 01:03:28 +0530

    added google maps

--- client/my-app/src/components/userpage/map.jsx
+++ client/my-app/src/components/userpage/map.jsx
@@ -1,27 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
-
-export default GoogleApiWrapper(
-  (props) => ({
-    apiKey: props.apiKey,
-    language: props.language,
-  }
-))(MapContainer)
+import { API_KEY } from '../../config/googlemaps';
 
 export class GoogleMap extends Component {
   render() {
     return (
       <Map google={this.props.google} zoom={14}>
         <Marker onClick={this.onMarkerClick} name={'Current location'} />
-
-        <InfoWindow onClose={this.onInfoWindowClose}>
-          <div>
-            <h1>{this.state.selectedPlace.name}</h1>
-          </div>
-        </InfoWindow>
+        <InfoWindow onClose={this.onInfoWindowClose}></InfoWindow>
       </Map>
     );
   }
 }
 
-export default GoogleMap;
+export default GoogleApiWrapper({
+  apiKey: API_KEY,
+})(GoogleMap);
