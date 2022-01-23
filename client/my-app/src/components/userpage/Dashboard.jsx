f7b2c4fe9b43901885702164cdf4303de54d09ac
sinking8 <ashwinblaze111@gmail.com>
2022-01-23 15:11:09 +0530

    added footer

--- client/my-app/src/components/userpage/Dashboard.jsx
+++ client/my-app/src/components/userpage/Dashboard.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Grid, GridItem } from '@chakra-ui/react';
+import { Grid, GridItem, Flex } from '@chakra-ui/react';
 
 import Wreckinfo from './wreckinfo';
 import Navbar from './navbar';
@@ -10,19 +10,25 @@ import Footer from './footer';
 export const Dashboard = () => {
   return (
     <>
-    <Navbar />
-     <Grid>
-      <GridItem colStart={1} colEnd={2} p={"40px"}>
+      <Navbar />
+      <Grid>
+        <GridItem colStart={1} colEnd={2} p={'40px'}>
           <GoogleMap />
-      </GridItem>
-          <GridItem colStart={1} colEnd={2} mt={"-80px"}>
-            <Wreckinfo />
-          </GridItem>
-          <GridItem rowstart={5}>
-          <Footer mt={"1000px"}/>
-          </GridItem>
+        </GridItem>
+        <GridItem colStart={1} colEnd={2} mt={'-80px'}>
+          <Wreckinfo />
+        </GridItem>
       </Grid>
-
+      <Flex
+        style={{
+          position: 'absolute',
+          marginTop: '900px',
+          marginLeft: '1px',
+        }}
+        className="flex"
+      >
+        <Footer />
+      </Flex>
     </>
   );
 };
